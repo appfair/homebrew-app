1eb847792a435248113057219ce881d63a09c819
appfairz <appfairz@appfair.net>
2021-10-21 11:25:12 -0400

    chore: checkpoint

---
+++ Casks/cloud-cuckoo.rb
@@ -0,0 +1,9 @@
+cask "cloud-cuckoo" do
+  name "Clous Cuckoo"
+  desc "The Cloud Cuckoo app"
+  homepage "https://github.com/Cloud-Cuckoo/App/"
+  app "Cloud Cuckoo.app", target: "App Fair/Cloud Cuckoo.app"
+  version "0.8.258"
+  url "https://github.com/Cloud-Cuckoo/App/releases/download/#{version}/Cloud-Cuckoo-macOS.zip"
+  sha256 "fcaa781c521cf7e4d67ffaa92d718cf182db3accad6930e64eae1d2740982dc5"
+end
