f528ccdc73f296d44b475e5ab34937a4b45d6180
appfairz <appfairz@appfair.net>
2021-10-21 11:19:47 -0400

    chore: checkpoint

--- Casks/tune-out.rb
+++ Casks/tune-out.rb
@@ -3,7 +3,7 @@ cask "tune-out" do
   desc "The Tune Out app"
   homepage "https://github.com/Tune-Out/App/"
   app "App Fair/Tune Out.app"
-  version "0.6.114"
+  version "0.8.258"
   url "https://github.com/Tune-Out/App/releases/download/#{version}/Tune-Out-macOS.zip"
   sha256 "37d8def975b7bdfc5760ec321b26430c7487a6dd916c50ac4e003e337e2ad3b8"
 end
