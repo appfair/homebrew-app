64d7088869c20d816fe1db3a656d879336427aac
appfairz <appfairz@appfair.net>
2021-10-21 15:18:58 -0400

    chore: checkpoint

--- Casks/app-fair.rb
+++ Casks/app-fair.rb
@@ -3,6 +3,7 @@ cask "app-fair" do
   desc "The App Fair Catalog Browser"
   homepage "https://appfair.net/"
   app "App Fair.app"
+  depends_on macos: ">= :monterey"
   version "0.6.114"
   url "https://github.com/App-Fair/App/releases/download/#{version}/App-Fair-macOS.zip"
   sha256 "37d8def975b7bdfc5760ec321b26430c7487a6dd916c50ac4e003e337e2ad3b8"
--- Casks/cloud-cuckoo.rb
+++ Casks/cloud-cuckoo.rb
@@ -1,10 +1,9 @@
 cask "cloud-cuckoo" do
-  name "Clous Cuckoo"
+  name "Cloud Cuckoo"
   desc "The Cloud Cuckoo app"
   homepage "https://github.com/Cloud-Cuckoo/App/"
   app "Cloud Cuckoo.app", target: "App Fair/Cloud Cuckoo.app"
   depends_on macos: ">= :monterey"
-  #depends_on cask: "app-fair", args: { no_quarantine: true }
   version "0.8.81"
   url "https://github.com/Cloud-Cuckoo/App/releases/download/#{version}/Cloud-Cuckoo-macOS.zip"
   sha256 "2f833e270826ac40b9d40416d002238767b9bbf9d598c1aa2558af318b4cb9f1"
