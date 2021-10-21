4c669c734382a4e64f3e31ad5732245e8c0430f5
appfairz <appfairz@appfair.net>
2021-10-21 10:54:21 -0400

    chore: checkpoint

---
+++ Casks/App-Fair.rb
@@ -0,0 +1,9 @@
+cask "App-Fair" do
+  name "App Fair"
+  desc "The App Fair Catalog Browser"
+  homepage "https://appfair.net/"
+  app "App Fair.app"
+  version "0.6.114"
+  url "https://github.com/App-Fair/App/releases/download/#{version}/App-Fair-macOS.zip"
+  sha256 "0b80bf62922291da391098f979683e69cc7b65c4bdb986a431e3f1d9175fba20"
+end
