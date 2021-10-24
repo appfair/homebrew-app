cb331067086fa8f13859c776df64ee85e664b78b
appfairbot <appfairbot@appfair.net>
2021-10-24 15:03:45 +0000

    cask update

--- Casks/bon-mot.rb
+++ Casks/bon-mot.rb
@@ -4,7 +4,7 @@ cask "bon-mot" do
   homepage "https://github.com/Bon-Mot/App/"
   app "Bon Mot.app", target: "App Fair/Bon Mot.app"
   depends_on macos: ">= :monterey"
-  version "0.0.7"
+  version "0.0.8"
   url "https://github.com/Bon-Mot/App/releases/download/#{version}/Bon-Mot-macOS.zip"
-  sha256 "a4142783bd84c819d238cb5fc93aebc971041de853b5517c601f149aeb0adf5c"
+  sha256 "2bc993172205c7cbb19f1be875451fba7757ffecaba3c1c7ceb0516e5d89ea2e"
 end
