e6d8a6b3e22de78d3e0d984900d13664b793a184
appfairbot <appfairbot@appfair.net>
2021-10-25 19:44:53 +0000

    cask update

--- Casks/app-fair.rb
+++ Casks/app-fair.rb
@@ -4,7 +4,7 @@ cask "app-fair" do
   homepage "https://github.com/App-Fair/App/"
   app "App Fair.app", target: "App Fair.app"
   depends_on macos: ">= :monterey"
-  version "0.6.116"
+  version "0.6.117"
   url "https://github.com/App-Fair/App/releases/download/#{version}/App-Fair-macOS.zip"
-  sha256 "4d2c30323b21034805c5c8398370588584eb4359a3101f3214263006154fd577"
+  sha256 "b2daa8bf9a36d884dc8bcffee22307f3f9b64142db95acef6e7dfa1ae03737d3"
 end
