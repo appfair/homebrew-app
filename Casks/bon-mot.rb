5645c3856bf227ceaec66fefc89f09e72fb72df1
appfairbot <appfairbot@appfair.net>
2021-10-25 19:31:06 +0000

    cask update

--- Casks/bon-mot.rb
+++ Casks/bon-mot.rb
@@ -4,7 +4,7 @@ cask "bon-mot" do
   homepage "https://github.com/Bon-Mot/App/"
   app "Bon Mot.app", target: "App Fair/Bon Mot.app"
   depends_on macos: ">= :monterey"
-  version "0.0.18"
+  version "0.0.21"
   url "https://github.com/Bon-Mot/App/releases/download/#{version}/Bon-Mot-macOS.zip"
-  sha256 "9e39a29e0643b1591fecbd6d8f124890f6b88a468b3bbbe2d816addb9ac2f6e6"
+  sha256 "9d94d9e9015314ce9406271ef0ee3a1de526528b807083f399ce1a81d410f630"
 end
--- Casks/cloud-cuckoo.rb
+++ Casks/cloud-cuckoo.rb
@@ -4,7 +4,7 @@ cask "cloud-cuckoo" do
   homepage "https://github.com/Cloud-Cuckoo/App/"
   app "Cloud Cuckoo.app", target: "App Fair/Cloud Cuckoo.app"
   depends_on macos: ">= :monterey"
-  version "0.8.82"
+  version "0.8.83"
   url "https://github.com/Cloud-Cuckoo/App/releases/download/#{version}/Cloud-Cuckoo-macOS.zip"
-  sha256 "cb1b47e17f590478e8d125acbabd9d1933f62fb22f8915d45103db1fa3505d14"
+  sha256 "ee1cdec90ca3f4e3f68adfe4888041c367aad1b57c79b150d90e3634556ddd40"
 end
