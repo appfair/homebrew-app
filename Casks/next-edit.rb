94e2e51605a8952487e7eb93756fde03cf6c5b4c
appfairbot <appfairbot@appfair.net>
2021-10-27 04:39:04 +0000

    cask update

--- Casks/next-edit.rb
+++ Casks/next-edit.rb
@@ -1,6 +1,6 @@
 cask "next-edit" do
-  version "0.0.73"
-  sha256 "0dc9f549c18cb2d59da639d75c03c7af51afae9e43c00d72f8e00fc81e331491"
+  version "0.0.77"
+  sha256 "f3264c2e0f34cf8645aa03de282b88db200d56241e94a7292dd2225db594f047"
 
   url "https://github.com/Next-Edit/App/releases/download/#{version}/Next-Edit-macOS.zip",
       verified: "github.com/Next-Edit/"
