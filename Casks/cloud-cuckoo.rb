fd4ec6197df83f19d35a5a86d5a7a3376b2894cf
appfairbot <appfairbot@appfair.net>
2021-10-28 23:54:21 +0000

    cask update

--- Casks/cloud-cuckoo.rb
+++ Casks/cloud-cuckoo.rb
@@ -1,6 +1,6 @@
 cask "cloud-cuckoo" do
-  version "0.8.94"
-  sha256 "eb7d0d059d7c661607246bf372f717340ae0d88ab81c171c4710a49e58976475"
+  version "0.8.96"
+  sha256 "44450d0b4b1919a7d0f0fcc1fdf8de0f9aa174eb487830a9c51bcd62ea03006d"
 
   url "https://github.com/Cloud-Cuckoo/App/releases/download/#{version}/Cloud-Cuckoo-macOS.zip",
       verified: "github.com/Cloud-Cuckoo/"
