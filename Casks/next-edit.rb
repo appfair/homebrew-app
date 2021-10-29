47d65d4f56a8ef89b5026e4eea99a14e5f1ba799
appfairbot <appfairbot@appfair.net>
2021-10-29 04:04:58 +0000

    cask update

---
+++ Casks/next-edit.rb
@@ -0,0 +1,28 @@
+cask "next-edit" do
+  version "0.0.99"
+  sha256 "83070f843b5c4bffaa1a0a5185c568245507ee701e5fdf2409f9ae5e9ffe937a"
+
+  url "https://github.com/Next-Edit/App/releases/download/#{version}/Next-Edit-macOS.zip",
+      verified: "github.com/Next-Edit/"
+  name "Next Edit"
+  desc "Syntactic Text Editor"
+  homepage "https://github.com/Next-Edit/App/"
+
+  depends_on macos: ">= :monterey"
+  depends_on cask: "app-fair"
+
+  app "Next Edit.app", target: "App Fair/Next Edit.app"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Next Edit.app"
+  end
+
+  uninstall quit: "app.Next-Edit"
+  zap trash: [
+    "~/Library/Caches/app.Next-Edit",
+    "~/Library/Containers/app.Next-Edit",
+    "~/Library/Preferences/app.Next-Edit.plist",
+    "~/Library/Application Scripts/app.Next-Edit",
+    "~/Library/Saved Application State/app.Next-Edit.savedState",
+  ]
+end
