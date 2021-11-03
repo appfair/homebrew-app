65cb4d25ca285ce6be1b975ec38faeb2eb7ded51
appfairbot <appfairbot@appfair.net>
2021-11-03 05:42:12 +0000

    cask update

---
+++ Casks/tune-out.rb
@@ -0,0 +1,29 @@
+cask "tune-out" do
+  version "0.8.310"
+  sha256 "705802b6d7400187788c5022e3ffb71ab88c5c804f749d1f03c2b48492575d8a"
+
+  url "https://github.com/Tune-Out/App/releases/download/#{version}/Tune-Out-macOS.zip",
+      verified: "github.com/Tune-Out/"
+  name "Tune Out"
+  desc "Stream Internet Radio"
+  homepage "https://github.com/Tune-Out/App/"
+
+  depends_on macos: ">= :monterey"
+  depends_on cask: "app-fair"
+
+  app "Tune Out.app", target: "App Fair/Tune Out.app"
+  binary "#{appdir}/App Fair/Tune Out.app/Contents/MacOS/Tune Out", target: "tune-out"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Tune Out.app"
+  end
+
+  uninstall quit: "app.Tune-Out"
+  zap trash: [
+    "~/Library/Caches/app.Tune-Out",
+    "~/Library/Containers/app.Tune-Out",
+    "~/Library/Preferences/app.Tune-Out.plist",
+    "~/Library/Application Scripts/app.Tune-Out",
+    "~/Library/Saved Application State/app.Tune-Out.savedState",
+  ]
+end
