840fe0dbec080dc14130aee198928bb87d6e1b7f
appfairbot <appfairbot@appfair.net>
2021-11-08 12:18:34 +0000

    cask update

--- Casks/tune-out.rb
+++ Casks/tune-out.rb
@@ -1,6 +1,6 @@
 cask "tune-out" do
-  version "0.8.317"
-  sha256 "dde637520ada5f76100971db8aa48b2f84acd5f6f607b64735e0775f365aa301"
+  version "0.8.318"
+  sha256 "67255351fede69656d38d158033129991feedbae18ee529c18e4d9c96bb6f38c"
 
   url "https://github.com/Tune-Out/App/releases/download/#{version}/Tune-Out-macOS.zip",
       verified: "github.com/Tune-Out/"
