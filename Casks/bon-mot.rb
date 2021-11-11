b32ee7269621c5c12d1aab3db22fde50ae148cfb
appfairbot <appfairbot@appfair.net>
2021-11-11 02:31:04 +0000

    cask update

---
+++ Casks/bon-mot.rb
@@ -0,0 +1,29 @@
+cask "bon-mot" do
+  version "0.0.78"
+  sha256 "7ffd256376b0fa8ce34341ef311d8e1f500729df534d940f209a41a73a710606"
+
+  url "https://github.com/Bon-Mot/App/releases/download/#{version}/Bon-Mot-macOS.zip",
+      verified: "github.com/Bon-Mot/"
+  name "Bon Mot"
+  desc "Book reader"
+  homepage "https://github.com/Bon-Mot/App/"
+
+  depends_on macos: ">= :monterey"
+  depends_on cask: "app-fair"
+
+  app "Bon Mot.app", target: "App Fair/Bon Mot.app"
+  binary "#{appdir}/App Fair/Bon Mot.app/Contents/MacOS/Bon Mot", target: "bon-mot"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Bon Mot.app"
+  end
+
+  uninstall quit: "app.Bon-Mot"
+  zap trash: [
+    "~/Library/Caches/app.Bon-Mot",
+    "~/Library/Containers/app.Bon-Mot",
+    "~/Library/Preferences/app.Bon-Mot.plist",
+    "~/Library/Application Scripts/app.Bon-Mot",
+    "~/Library/Saved Application State/app.Bon-Mot.savedState",
+  ]
+end
