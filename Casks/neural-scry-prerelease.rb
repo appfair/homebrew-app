7d0a7624f7bb2ef53324dfeb39af6af746b070a1
appfairbot <appfairbot@appfair.net>
2021-11-20 17:39:17 +0000

    cask update

---
+++ Casks/neural-scry-prerelease.rb
@@ -0,0 +1,28 @@
+cask "neural-scry-prerelease" do
+  version "0.0.10"
+  sha256 "78111f1b767042f642dccdb397c7d684d31da5632fdc5ca3f072d47baa5bc25d"
+
+  url "https://github.com/Neural-Scry/App/releases/download/#{version}/Neural-Scry-macOS.zip",
+      verified: "github.com/Neural-Scry/"
+  name "Neural Scry"
+  desc "A handy and useful artists tool"
+  homepage "https://github.com/Neural-Scry/App/"
+
+  depends_on macos: ">= :monterey"
+  depends_on cask: "app-fair"
+
+  app "Neural Scry.app", target: "App Fair/Neural Scry.app"
+  binary "#{appdir}/App Fair/Neural Scry.app/Contents/MacOS/Neural Scry", target: "neural-scry-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Neural Scry.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Neural-Scry",
+    "~/Library/Containers/app.Neural-Scry",
+    "~/Library/Preferences/app.Neural-Scry.plist",
+    "~/Library/Application Scripts/app.Neural-Scry",
+    "~/Library/Saved Application State/app.Neural-Scry.savedState",
+  ]
+end
