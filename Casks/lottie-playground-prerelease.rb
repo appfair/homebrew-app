4591afaadc54ac75887b39f4a2455e003501dd16
appfairbot <appfairbot@appfair.net>
2021-11-25 13:10:30 +0000

    cask update

---
+++ Casks/lottie-playground-prerelease.rb
@@ -0,0 +1,28 @@
+cask "lottie-playground-prerelease" do
+  version "0.0.9"
+  sha256 "6ca2aab5f9fc9d28abf39a7c67bfb403cb98a56cc121e2255ef5c25c875801d3"
+
+  url "https://github.com/Lottie-Playground/App/releases/download/#{version}/Lottie-Playground-macOS.zip",
+      verified: "github.com/Lottie-Playground/"
+  name "Lottie Playground"
+  desc "Preview and experiment with Lottie animations"
+  homepage "https://github.com/Lottie-Playground/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Lottie Playground.app", target: "App Fair/Lottie Playground.app"
+  binary "#{appdir}/App Fair/Lottie Playground.app/Contents/MacOS/Lottie Playground", target: "lottie-playground-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Lottie Playground.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Lottie-Playground",
+    "~/Library/Containers/app.Lottie-Playground",
+    "~/Library/Preferences/app.Lottie-Playground.plist",
+    "~/Library/Application Scripts/app.Lottie-Playground",
+    "~/Library/Saved Application State/app.Lottie-Playground.savedState",
+  ]
+end
