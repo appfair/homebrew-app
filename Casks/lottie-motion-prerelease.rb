1f1336c69302627b67fb34097b7cf205b1f366a1
appfairbot <appfairbot@appfair.net>
2021-11-26 00:31:32 +0000

    cask update

---
+++ Casks/lottie-motion-prerelease.rb
@@ -0,0 +1,28 @@
+cask "lottie-motion-prerelease" do
+  version "0.0.14"
+  sha256 "95dd78f445dc7b1d2e59e4e1b86ef9ca4af339d32bba071fb894db20492a938c"
+
+  url "https://github.com/Lottie-Motion/App/releases/download/#{version}/Lottie-Motion-macOS.zip",
+      verified: "github.com/Lottie-Motion/"
+  name "Lottie Motion"
+  desc "Preview and experiment with Lottie animations"
+  homepage "https://github.com/Lottie-Motion/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Lottie Motion.app", target: "App Fair/Lottie Motion.app"
+  binary "#{appdir}/App Fair/Lottie Motion.app/Contents/MacOS/Lottie Motion", target: "lottie-motion-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Lottie Motion.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Lottie-Motion",
+    "~/Library/Containers/app.Lottie-Motion",
+    "~/Library/Preferences/app.Lottie-Motion.plist",
+    "~/Library/Application Scripts/app.Lottie-Motion",
+    "~/Library/Saved Application State/app.Lottie-Motion.savedState",
+  ]
+end
