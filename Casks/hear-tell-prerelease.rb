fe4d4d253707007e1f6916fe3a0be6e0f0235480
appfairbot <appfairbot@appfair.net>
2021-11-28 02:11:19 +0000

    cask update

---
+++ Casks/hear-tell-prerelease.rb
@@ -0,0 +1,28 @@
+cask "hear-tell-prerelease" do
+  version "0.2.10"
+  sha256 "28470bc64cb081dd1a8dea0e9830965e18603842439f07e5e02cd151b1bc3731"
+
+  url "https://github.com/Hear-Tell/App/releases/download/#{version}/Hear-Tell-macOS.zip",
+      verified: "github.com/Hear-Tell/"
+  name "Hear Tell"
+  desc "Identify ambient sounds."
+  homepage "https://github.com/Hear-Tell/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Hear Tell.app", target: "App Fair/Hear Tell.app"
+  binary "#{appdir}/App Fair/Hear Tell.app/Contents/MacOS/Hear Tell", target: "hear-tell-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Hear Tell.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Hear-Tell",
+    "~/Library/Containers/app.Hear-Tell",
+    "~/Library/Preferences/app.Hear-Tell.plist",
+    "~/Library/Application Scripts/app.Hear-Tell",
+    "~/Library/Saved Application State/app.Hear-Tell.savedState",
+  ]
+end
