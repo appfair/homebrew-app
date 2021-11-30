07b23cf288b085e8b1349a47a8e0e4b4f8ff6bd0
appfairbot <appfairbot@appfair.net>
2021-11-30 15:27:35 +0000

    cask update

--- Casks/app-fair-prerelease.rb
+++ Casks/app-fair-prerelease.rb
@@ -1,6 +1,6 @@
 cask "app-fair-prerelease" do
-  version "0.6.248"
-  sha256 "c626f1114947bf08742d2733f924b1cfa00fe18fb5e199ec18e6a6889aeea4bb"
+  version "0.6.249"
+  sha256 "d91dad606c121989f3bf251c70cbc965eae3181e41ad1cfc1fe7cbd542bccc03"
 
   url "https://github.com/App-Fair/App/releases/download/#{version}/App-Fair-macOS.zip",
       verified: "github.com/App-Fair/"
