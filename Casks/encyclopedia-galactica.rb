e195ba98df365fe32f3e62c100af1a47c1bc7239
appfairbot <appfairbot@appfair.net>
2021-12-01 23:47:15 +0000

    cask update

---
+++ Casks/encyclopedia-galactica.rb
@@ -0,0 +1,28 @@
+cask "encyclopedia-galactica" do
+  version "0.0.21"
+  sha256 "b16fce2c5d3f5156f62ce333295719e77bb0ca85d06933c7440c2ff1c6f46646"
+
+  url "https://github.com/Encyclopedia-Galactica/App/releases/download/#{version}/Encyclopedia-Galactica-macOS.zip",
+      verified: "github.com/Encyclopedia-Galactica/"
+  name "Encyclopedia Galactica"
+  desc "All the accumulated knowledge in the galaxy"
+  homepage "https://github.com/Encyclopedia-Galactica/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Encyclopedia Galactica.app", target: "App Fair/Encyclopedia Galactica.app"
+  binary "#{appdir}/App Fair/Encyclopedia Galactica.app/Contents/MacOS/Encyclopedia Galactica", target: "encyclopedia-galactica"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Encyclopedia Galactica.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Encyclopedia-Galactica",
+    "~/Library/Containers/app.Encyclopedia-Galactica",
+    "~/Library/Preferences/app.Encyclopedia-Galactica.plist",
+    "~/Library/Application Scripts/app.Encyclopedia-Galactica",
+    "~/Library/Saved Application State/app.Encyclopedia-Galactica.savedState",
+  ]
+end
