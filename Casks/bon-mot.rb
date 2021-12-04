8bd2be53305d1796b73748d2e925395b19294e4e
appfairbot <appfairbot@appfair.net>
2021-12-04 02:37:13 +0000

    cask update

---
+++ Casks/bon-mot.rb
@@ -0,0 +1,28 @@
+cask "bon-mot" do
+  version "0.2.13"
+  sha256 "89d5a58b2a709f7733648a77a410ad01b0a333ac573a9d1c68d77682f6b4c587"
+
+  url "https://github.com/Bon-Mot/App/releases/download/#{version}/Bon-Mot-macOS.zip",
+      verified: "github.com/Bon-Mot/"
+  name "Bon Mot"
+  desc "Simple dictionary app"
+  homepage "https://github.com/Bon-Mot/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Bon Mot.app", target: "App Fair/Bon Mot.app"
+  binary "#{appdir}/App Fair/Bon Mot.app/Contents/MacOS/Bon Mot", target: "bon-mot"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Bon Mot.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Bon-Mot",
+    "~/Library/Containers/app.Bon-Mot",
+    "~/Library/Preferences/app.Bon-Mot.plist",
+    "~/Library/Application Scripts/app.Bon-Mot",
+    "~/Library/Saved Application State/app.Bon-Mot.savedState",
+  ]
+end
