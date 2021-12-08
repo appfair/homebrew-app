80a5cab34f4ebc8280458313497f4f68b1e132e6
appfairbot <appfairbot@appfair.net>
2021-12-08 00:44:00 +0000

    cask update

---
+++ Casks/app-fair-prerelease.rb
@@ -0,0 +1,28 @@
+cask "app-fair-prerelease" do
+  version "0.7.16"
+  sha256 "d1a4e76da38213326f1fe1b686633df79550c4fdc73f6a94bf8545327cdd6f41"
+
+  url "https://github.com/App-Fair/App/releases/download/#{version}/App-Fair-macOS.zip",
+      verified: "github.com/App-Fair/"
+  name "App Fair"
+  desc "The App Fair Catalog Browser"
+  homepage "https://github.com/App-Fair/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "App Fair.app", target: "App Fair.app"
+  binary "#{appdir}/App Fair.app/Contents/MacOS/App Fair", target: "app-fair-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.App-Fair",
+    "~/Library/Containers/app.App-Fair",
+    "~/Library/Preferences/app.App-Fair.plist",
+    "~/Library/Application Scripts/app.App-Fair",
+    "~/Library/Saved Application State/app.App-Fair.savedState",
+  ]
+end
