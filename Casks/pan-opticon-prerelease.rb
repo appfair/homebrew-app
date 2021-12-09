ce104b87c01749e10625550935c4ca5693e4b30b
appfairbot <appfairbot@appfair.net>
2021-12-09 13:23:30 +0000

    cask update

---
+++ Casks/pan-opticon-prerelease.rb
@@ -0,0 +1,28 @@
+cask "pan-opticon-prerelease" do
+  version "0.2.9"
+  sha256 "3b1c116a50dc35cc9984232706c2958b33548498f9cb88b48c9e865834c883e5"
+
+  url "https://github.com/Pan-Opticon/App/releases/download/#{version}/Pan-Opticon-macOS.zip",
+      verified: "github.com/Pan-Opticon/"
+  name "Pan Opticon"
+  desc "Watch the Watchers"
+  homepage "https://github.com/Pan-Opticon/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Pan Opticon.app", target: "App Fair/Pan Opticon.app"
+  binary "#{appdir}/App Fair/Pan Opticon.app/Contents/MacOS/Pan Opticon", target: "pan-opticon-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Pan Opticon.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Pan-Opticon",
+    "~/Library/Containers/app.Pan-Opticon",
+    "~/Library/Preferences/app.Pan-Opticon.plist",
+    "~/Library/Application Scripts/app.Pan-Opticon",
+    "~/Library/Saved Application State/app.Pan-Opticon.savedState",
+  ]
+end
