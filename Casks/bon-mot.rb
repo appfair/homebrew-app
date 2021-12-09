0dc5c10aa75489543c48c7445e324b3600f175f2
appfairbot <appfairbot@appfair.net>
2021-12-09 08:59:49 +0000

    cask update

---
+++ Casks/bon-mot.rb
@@ -0,0 +1,28 @@
+cask "bon-mot" do
+  version "0.2.23"
+  sha256 "60af2200fe86b4acae2468858bca608c0a5bc1ebbdcfed36894bb3cd36bfd8b4"
+
+  url "https://github.com/Bon-Mot/App/releases/download/#{version}/Bon-Mot-macOS.zip",
+      verified: "github.com/Bon-Mot/"
+  name "Bon Mot"
+  desc "Simple dictionary app"
+  homepage "https://github.com/Bon-Mot/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Bon Mot.app", target: "App Fair/Bon Mot.app"
+  binary "#{appdir}/App Fair/Bon Mot.app/Contents/MacOS/Bon Mot", target: "bon-mot"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Bon Mot.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Bon-Mot",
+    "~/Library/Containers/app.Bon-Mot",
+    "~/Library/Preferences/app.Bon-Mot.plist",
+    "~/Library/Application Scripts/app.Bon-Mot",
+    "~/Library/Saved Application State/app.Bon-Mot.savedState",
+  ]
+end
---
+++ Casks/pan-opticon.rb
@@ -0,0 +1,28 @@
+cask "pan-opticon" do
+  version "0.2.8"
+  sha256 "a24fb905074b39da0c2c8950d6cc070bb18f1c479f21a6028c705e94057311e1"
+
+  url "https://github.com/Pan-Opticon/App/releases/download/#{version}/Pan-Opticon-macOS.zip",
+      verified: "github.com/Pan-Opticon/"
+  name "Pan Opticon"
+  desc "Watch the Watchers"
+  homepage "https://github.com/Pan-Opticon/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Pan Opticon.app", target: "App Fair/Pan Opticon.app"
+  binary "#{appdir}/App Fair/Pan Opticon.app/Contents/MacOS/Pan Opticon", target: "pan-opticon"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Pan Opticon.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Pan-Opticon",
+    "~/Library/Containers/app.Pan-Opticon",
+    "~/Library/Preferences/app.Pan-Opticon.plist",
+    "~/Library/Application Scripts/app.Pan-Opticon",
+    "~/Library/Saved Application State/app.Pan-Opticon.savedState",
+  ]
+end
