9456617b89f4f3e3f81c8cfb5b12d3709c66f758
appfairbot <appfairbot@appfair.net>
2021-12-22 01:37:46 +0000

    cask update

---
+++ Casks/trivial-quiz-prerelease.rb
@@ -0,0 +1,28 @@
+cask "trivial-quiz-prerelease" do
+  version "0.0.1"
+  sha256 "7591331f10ed88c932c83859ed227faf85ebfb9bbe137f48384671379971f8e8"
+
+  url "https://github.com/Trivial-Quiz/App/releases/download/#{version}/Trivial-Quiz-macOS.zip",
+      verified: "github.com/Trivial-Quiz/"
+  name "Trivial Quiz"
+  desc "Fork to make an App Fair App"
+  homepage "https://github.com/Trivial-Quiz/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Trivial Quiz.app", target: "App Fair/Trivial Quiz.app"
+  binary "#{appdir}/App Fair/Trivial Quiz.app/Contents/MacOS/Trivial Quiz", target: "trivial-quiz-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Trivial Quiz.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Trivial-Quiz",
+    "~/Library/Containers/app.Trivial-Quiz",
+    "~/Library/Preferences/app.Trivial-Quiz.plist",
+    "~/Library/Application Scripts/app.Trivial-Quiz",
+    "~/Library/Saved Application State/app.Trivial-Quiz.savedState",
+  ]
+end
