ec022fd189d9f8a6ea42bc42142e475164af632a
appfairbot <appfairbot@appfair.net>
2021-12-31 15:45:58 +0000

    cask update

---
+++ Casks/sita-sings-the-blues-prerelease.rb
@@ -0,0 +1,28 @@
+cask "sita-sings-the-blues-prerelease" do
+  version "0.0.1"
+  sha256 "ed81f89968500e326bac06bf5a32ef2e1d59f71178bcf8abb53994c4fe859e8e"
+
+  url "https://github.com/Sita-Sings-the-Blues/App/releases/download/#{version}/Sita-Sings-the-Blues-macOS.zip",
+      verified: "github.com/Sita-Sings-the-Blues/"
+  name "Sita Sings the Blues"
+  desc "The movie 'Site Sings the Blues'"
+  homepage "https://github.com/Sita-Sings-the-Blues/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Sita Sings the Blues.app", target: "App Fair/Sita Sings the Blues.app"
+  binary "#{appdir}/App Fair/Sita Sings the Blues.app/Contents/MacOS/Sita Sings the Blues", target: "sita-sings-the-blues-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Sita Sings the Blues.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Sita-Sings-the-Blues",
+    "~/Library/Containers/app.Sita-Sings-the-Blues",
+    "~/Library/Preferences/app.Sita-Sings-the-Blues.plist",
+    "~/Library/Application Scripts/app.Sita-Sings-the-Blues",
+    "~/Library/Saved Application State/app.Sita-Sings-the-Blues.savedState",
+  ]
+end
