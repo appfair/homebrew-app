0db3ee8d3eb21eb38b5b0c2597767ac3e52095f8
appfairbot <appfairbot@appfair.net>
2022-01-02 17:21:53 +0000

    cask update

--- Casks/sita-sings-the-blues.rb
+++ Casks/sita-sings-the-blues.rb
@@ -1,6 +1,6 @@
 cask "sita-sings-the-blues" do
-  version "0.0.21"
-  sha256 "42412a33baa03d9e62e3be192f21b93a23d330211d26382dafa9df4891c96b65"
+  version "0.0.23"
+  sha256 "a1a50369042cacfd9367894cf53ea8674b41fc8a0dd09c5099776cac6229a9bb"
 
   url "https://github.com/Sita-Sings-the-Blues/App/releases/download/#{version}/Sita-Sings-the-Blues-macOS.zip",
       verified: "github.com/Sita-Sings-the-Blues/"
