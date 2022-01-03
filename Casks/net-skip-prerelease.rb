c1f5cc2f47e5e8a636c3101017d8ebbfd5ea3f35
appfairbot <appfairbot@appfair.net>
2022-01-03 16:43:56 +0000

    cask update

---
+++ Casks/net-skip-prerelease.rb
@@ -0,0 +1,28 @@
+cask "net-skip-prerelease" do
+  version "0.0.1"
+  sha256 "839d339a9279781e2f441f7a22637538434b7ffbf31f3845578f7dca6f0efee7"
+
+  url "https://github.com/Net-Skip/App/releases/download/#{version}/Net-Skip-macOS.zip",
+      verified: "github.com/Net-Skip/"
+  name "Net Skip"
+  desc "Web browsing for the connoisseur"
+  homepage "https://github.com/Net-Skip/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Net Skip.app", target: "App Fair/Net Skip.app"
+  binary "#{appdir}/App Fair/Net Skip.app/Contents/MacOS/Net Skip", target: "net-skip-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Net Skip.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Net-Skip",
+    "~/Library/Containers/app.Net-Skip",
+    "~/Library/Preferences/app.Net-Skip.plist",
+    "~/Library/Application Scripts/app.Net-Skip",
+    "~/Library/Saved Application State/app.Net-Skip.savedState",
+  ]
+end
