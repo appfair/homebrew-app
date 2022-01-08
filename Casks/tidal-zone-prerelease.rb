f956fe156f654fe9fdfdee37ec4937392ef39703
appfairbot <appfairbot@appfair.net>
2022-01-08 20:32:47 +0000

    cask update

--- Casks/tidal-zone-prerelease.rb
+++ Casks/tidal-zone-prerelease.rb
@@ -1,6 +1,6 @@
 cask "tidal-zone-prerelease" do
-  version "0.0.1"
-  sha256 "dbe6b1b037604023555ae6124503e73439c93239f7fb2b90e32858932c6e4a82"
+  version "0.0.3"
+  sha256 "6ad617ac23ad06edd81489c391e0e8739f8d1b8fe240d41c59c967d1b680a7ff"
 
   url "https://github.com/Tidal-Zone/App/releases/download/#{version}/Tidal-Zone-macOS.zip",
       verified: "github.com/Tidal-Zone/"
