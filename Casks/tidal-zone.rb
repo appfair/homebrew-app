a7e13eb0404dddc4404f696ca82bb8246241a32b
appfairbot <appfairbot@appfair.net>
2022-01-09 15:47:34 +0000

    cask update

--- Casks/tidal-zone.rb
+++ Casks/tidal-zone.rb
@@ -1,6 +1,6 @@
-cask "tidal-zone-prerelease" do
-  version "0.0.5"
-  sha256 "67b41bf78b32bc7d55be18fcae2ada4432a230942806606bc97081eb8d6d7731"
+cask "tidal-zone" do
+  version "0.0.6"
+  sha256 "b6263c7267e885f943715a533ca188973d86add7f9bb4b4fc6815ecf92346ebc"
 
   url "https://github.com/Tidal-Zone/App/releases/download/#{version}/Tidal-Zone-macOS.zip",
       verified: "github.com/Tidal-Zone/"
@@ -12,7 +12,7 @@ cask "tidal-zone-prerelease" do
   
 
   app "Tidal Zone.app", target: "App Fair/Tidal Zone.app"
-  binary "#{appdir}/App Fair/Tidal Zone.app/Contents/MacOS/Tidal Zone", target: "tidal-zone-prerelease"
+  binary "#{appdir}/App Fair/Tidal Zone.app/Contents/MacOS/Tidal Zone", target: "tidal-zone"
 
   postflight do
     system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Tidal Zone.app"
