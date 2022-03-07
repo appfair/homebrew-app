5678fa06cf4a8d4af9e904ca16cc41282703c922
appfairbot <appfairbot@appfair.net>
2022-03-07 22:46:34 +0000

    cask update

---
+++ Casks/blunder-busq-prerelease.rb
@@ -0,0 +1,28 @@
+cask "blunder-busq-prerelease" do
+  version "0.0.6"
+  sha256 "4b5989ea972ca015221d6be5f7be43ba7de7c55931277fad595dc4f26638d37d"
+
+  url "https://github.com/Blunder-Busq/App/releases/download/#{version}/Blunder-Busq-macOS.zip",
+      verified: "github.com/Blunder-Busq/"
+  name "Blunder Busq"
+  desc "App for managing iOS devices"
+  homepage "https://github.com/Blunder-Busq/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Blunder Busq.app", target: "App Fair/Blunder Busq.app"
+  binary "#{appdir}/App Fair/Blunder Busq.app/Contents/MacOS/Blunder Busq", target: "blunder-busq-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Blunder Busq.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Blunder-Busq",
+    "~/Library/Containers/app.Blunder-Busq",
+    "~/Library/Preferences/app.Blunder-Busq.plist",
+    "~/Library/Application Scripts/app.Blunder-Busq",
+    "~/Library/Saved Application State/app.Blunder-Busq.savedState",
+  ]
+end
