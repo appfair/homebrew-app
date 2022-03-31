c568ce9a2904f495462c0deb967d69d1f8e979b6
appfairbot <appfairbot@appfair.net>
2022-03-31 13:45:53 +0000

    cask update

--- Casks/net-skip.rb
+++ Casks/net-skip.rb
@@ -1,6 +1,6 @@
 cask "net-skip" do
-  version "0.0.11"
-  sha256 "bf78732240087d7e0404cb09b35953c325886489ee31b1b4f3c66b89b08b2fad"
+  version "0.0.13"
+  sha256 "81098c65ef85f57b6db036dc1310db225355f1c28c829a326832f63d4dfd9a12"
 
   url "https://github.com/Net-Skip/App/releases/download/#{version}/Net-Skip-macOS.zip",
       verified: "github.com/Net-Skip/"
