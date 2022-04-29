a2ea6ba42a248e2fb1f484f22a3b9b8204d5a70a
appfairbot <appfairbot@appfair.net>
2022-04-29 20:38:01 +0000

    cask update

---
+++ Casks/stanza-redux-prerelease.rb
@@ -0,0 +1,28 @@
+cask "stanza-redux-prerelease" do
+  version "0.3.10"
+  sha256 "a19e9f971ab2278ef3ce99e5f4ca61ea302df1af9ea080af212595bbd2d1f83c"
+
+  url "https://github.com/Stanza-Redux/App/releases/download/#{version}/Stanza-Redux-macOS.zip",
+      verified: "github.com/Stanza-Redux/"
+  name "Stanza Redux"
+  desc "An electronic book reader"
+  homepage "https://github.com/Stanza-Redux/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Stanza Redux.app", target: "App Fair/Stanza Redux.app"
+  binary "#{appdir}/App Fair/Stanza Redux.app/Contents/MacOS/Stanza Redux", target: "stanza-redux-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Stanza Redux.app"
+  end
+
+  zap trash: [
+    "~/Library/Caches/app.Stanza-Redux",
+    "~/Library/Containers/app.Stanza-Redux",
+    "~/Library/Preferences/app.Stanza-Redux.plist",
+    "~/Library/Application Scripts/app.Stanza-Redux",
+    "~/Library/Saved Application State/app.Stanza-Redux.savedState",
+  ]
+end
