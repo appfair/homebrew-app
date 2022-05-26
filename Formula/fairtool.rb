55600c3761862b4a3ad2d4aa42e35334f862d6d9
appfairz <appfairz@appfair.net>
2022-05-26 07:50:19 -0400

    chore: checkpoint

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -2,15 +2,17 @@ class Fairtool < Formula
   desc "Manage a fair-ground ecosystem of apps"
   homepage "https://fair-ground.org"
   url "https://github.com/fair-ground/Fair.git", branch: "main"
+  # TODO: url "https://github.com/fair-ground/Fair/archive/0.9.0.tar.gz"
+  # TODO: sha256 "a098a48e8761d3aef9abd1a673b8b28b4b35f8dbad15218125e18461104874ca"
   version "latest"
-  head "https://github.com/fair-ground/Fair.git", branch: "main"
   license "AGPL-3.0-or-later"
+  head "https://github.com/fair-ground/Fair.git", branch: "main"
 
   depends_on "swift"
   uses_from_macos "swift"
 
   def install
-    system "swift", "build", "--disable-sandbox", "--configuration", "release"
+    system "swift", "build", "--disable-sandbox", "-c", "debug"
     bin.install ".build/release/fairtool"
   end
 
