dd5293297e8d48af6477c933b3ba52f03e9f158f
appfairz <appfairz@appfair.net>
2022-05-26 08:42:14 -0400

    chore: checkpoint

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -2,21 +2,20 @@ class Fairtool < Formula
   desc "Manage a fair-ground ecosystem of apps"
   homepage "https://fair-ground.org"
   url "https://github.com/fair-ground/Fair.git", branch: "main"
-  # TODO: url "https://github.com/fair-ground/Fair/archive/0.9.0.tar.gz"
-  # TODO: sha256 "a098a48e8761d3aef9abd1a673b8b28b4b35f8dbad15218125e18461104874ca"
   version "latest"
   license "AGPL-3.0-or-later"
   head "https://github.com/fair-ground/Fair.git", branch: "main"
 
   depends_on "swift"
+  uses_from_macos "swift", since: :big_sur # Swift 5.5.0
 
   def install
     # release build crashes swift 5.6.1 on macOS with the error:
     # "(FragmentOffset >= OffsetInBits && "overlapping or duplicate fragments")"
-    system "swift", "build", "--disable-sandbox", "-c", "release", "--product", "fairtool", "--verbose"
-    bin.install ".build/release/fairtool"
-    # system "swift", "build", "--disable-sandbox", "-c", "debug", "--product", "fairtool"
-    # bin.install ".build/debug/fairtool"
+    # system "swift", "build", "--disable-sandbox", "-c", "release", "--product", "fairtool", "--verbose"
+    # bin.install ".build/release/fairtool"
+    system "swift", "build", "--disable-sandbox", "-c", "debug", "--product", "fairtool"
+    bin.install ".build/debug/fairtool"
   end
 
   test do
