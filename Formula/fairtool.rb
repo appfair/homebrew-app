44e27e5b6f25372daa0c7c5023725c85159fe138
appfairz <appfairz@appfair.net>
2022-06-08 15:09:32 -0400

    chore: checkpoint

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -16,6 +16,9 @@ class Fairtool < Formula
   end
 
   test do
-    assert_match "Welcome", shell_output("#{bin}/fairtool welcome")
+    assert_match /^fairtool [0-9]+\.[0-9]+\.[0-9]+$/, shell_output("#{bin}/fairtool version 2>&1").strip
+    if OS.mac?
+      shell_output("#{bin}/fairtool app info --assemble /System/Applications/Calendar.app | jq -e '.[].entitlements[0][\"com.apple.security.app-sandbox\"]'")
+    end
   end
 end
