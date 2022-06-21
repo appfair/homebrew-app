c911a5166c8617b99509456c2981a84047bd4746
appfairbot <appfairbot@appfair.net>
2022-06-21 19:13:43 +0000

    fairtool cask update: 0.3.13

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -1,7 +1,7 @@
 class Fairtool < Formula
   desc "Manage an ecosystem of ipa app sources"
   homepage "https://github.com/fair-ground/Fair"
-  url "https://github.com/fair-ground/Fair/archive/0.3.12.tar.gz"
+  url "https://github.com/fair-ground/Fair/archive/0.3.13.tar.gz"
   sha256 "fef4bd56a65fd082343a629b304ed8e3497b4ff7a5c2875d8fec26326b3d7cbb"
   license "AGPL-3.0"
   head "https://github.com/fair-ground/Fair.git", branch: "main"
