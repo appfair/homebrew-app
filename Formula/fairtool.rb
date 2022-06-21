5d5c3f3f967fdfd266200ecbc6d8163bc0ca91bf
appfairz <appfairz@appfair.net>
2022-06-21 15:18:28 -0400

    chore: checkpoint

--- Formula/fairtool.rb
+++ Formula/fairtool.rb
@@ -2,7 +2,7 @@ class Fairtool < Formula
   desc "Manage an ecosystem of ipa app sources"
   homepage "https://github.com/fair-ground/Fair"
   url "https://github.com/fair-ground/Fair/archive/0.3.13.tar.gz"
-  sha256 "fef4bd56a65fd082343a629b304ed8e3497b4ff7a5c2875d8fec26326b3d7cbb"
+  sha256 "31e00759af1fa941a65fb8908403a14729b12e01a08df3c1cc753826670c7f26"
   license "AGPL-3.0"
   head "https://github.com/fair-ground/Fair.git", branch: "main"
 
