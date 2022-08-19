3eea1364d78fbec3fe9525f21284b1e15587bbcf
appfairbot <appfairbot@appfair.net>
2022-08-19 14:21:05 +0000

    cask update

---
+++ Casks/cloud-cuckoo-prerelease.rb
@@ -0,0 +1,31 @@
+cask "cloud-cuckoo-prerelease" do
+  version "0.9.225"
+  sha256 "21b920609c738522d1273dda8f67d1e957cff3c0c26f478819598737c8502384"
+
+  url "https://github.com/Cloud-Cuckoo/App/releases/download/#{version}/Cloud-Cuckoo-macOS.zip",
+      verified: "github.com/Cloud-Cuckoo/"
+  name "Cloud Cuckoo"
+  desc "A whimsical game of excitement and delight"
+  homepage "https://github.com/Cloud-Cuckoo/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Cloud Cuckoo.app", target: "App Fair/Cloud Cuckoo.app"
+  binary "#{appdir}/App Fair/Cloud Cuckoo.app/Contents/MacOS/Cloud Cuckoo", target: "cloud-cuckoo-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Cloud Cuckoo.app"
+  end
+
+  zap trash: [
+    ~/Library/Application Scripts/app.Cloud-Cuckoo,
+    ~/Library/Application Support/app.Cloud-Cuckoo,
+    ~/Library/Caches/app.Cloud-Cuckoo,
+    ~/Library/Containers/app.Cloud-Cuckoo,
+    ~/Library/HTTPStorages/app.Cloud-Cuckoo,
+    ~/Library/HTTPStorages/app.Cloud-Cuckoo.binarycookies,
+    ~/Library/Preferences/app.Cloud-Cuckoo.plist,
+    ~/Library/Saved Application State/app.Cloud-Cuckoo.savedState
+  ]
+end
