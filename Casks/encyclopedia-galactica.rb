5b1e81ea338cda7456bbda9400e1062e83b97a6b
appfairbot <appfairbot@appfair.net>
2022-08-31 22:01:49 +0000

    cask update

--- Casks/encyclopedia-galactica.rb
+++ Casks/encyclopedia-galactica.rb
@@ -1,6 +1,6 @@
 cask "encyclopedia-galactica" do
-  version "0.1.59"
-  sha256 "653f71bad2950d5b2b8b9bdb5e104254ef4d48f6f2c34ae513c76bdeb36929b1"
+  version "0.1.60"
+  sha256 "e3f2c2c4a5d53344a6122d4f2260f14a7653bc21b69c76aa4cb6ca867f36c3f6"
 
   url "https://github.com/Encyclopedia-Galactica/App/releases/download/#{version}/Encyclopedia-Galactica-macOS.zip",
       verified: "github.com/Encyclopedia-Galactica/"
