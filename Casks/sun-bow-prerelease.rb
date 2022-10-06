82987a683a37df6942a68fde47aa57578e67ffd1
appfairbot <appfairbot@appfair.net>
2022-10-06 20:42:23 +0000

    cask update

---
+++ Casks/sun-bow-prerelease.rb
@@ -0,0 +1,31 @@
+cask "sun-bow-prerelease" do
+  version "0.0.28"
+  sha256 "efe1825a730acf239513d7ae58270afd5e106d8dcf96cd120385057bc60b448c"
+
+  url "https://github.com/Sun-Bow/App/releases/download/#{version}/Sun-Bow-macOS.zip",
+      verified: "github.com/Sun-Bow/"
+  name "Sun Bow"
+  desc "An app for sunnier days"
+  homepage "https://github.com/Sun-Bow/App/"
+
+  depends_on macos: ">= :monterey"
+  
+
+  app "Sun Bow.app", target: "App Fair/Sun Bow.app"
+  binary "#{appdir}/App Fair/Sun Bow.app/Contents/MacOS/Sun Bow", target: "sun-bow-prerelease"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/App Fair/Sun Bow.app"
+  end
+
+  zap trash: [
+    ~/Library/Application Scripts/app.Sun-Bow,
+    ~/Library/Application Support/app.Sun-Bow,
+    ~/Library/Caches/app.Sun-Bow,
+    ~/Library/Containers/app.Sun-Bow,
+    ~/Library/HTTPStorages/app.Sun-Bow,
+    ~/Library/HTTPStorages/app.Sun-Bow.binarycookies,
+    ~/Library/Preferences/app.Sun-Bow.plist,
+    ~/Library/Saved Application State/app.Sun-Bow.savedState
+  ]
+end
