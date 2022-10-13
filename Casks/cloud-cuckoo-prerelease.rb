19b2ea44d43332c9ebae0ac3ea0f606e5c47beea
appfairbot <appfairbot@appfair.net>
2022-10-13 21:33:22 +0000

    cask update

--- Casks/cloud-cuckoo-prerelease.rb
+++ Casks/cloud-cuckoo-prerelease.rb
@@ -1,6 +1,6 @@
 cask "cloud-cuckoo-prerelease" do
   version "0.9.330"
-  sha256 "60633cafc85b68c963c7af0d7592172e8bfcceb0d0beddb9c17062ef71084b7e"
+  sha256 "2dd5cb06555d35b13dd05a51b949339cd16b62f363e0845090ee2d5cc432f0d3"
 
   url "https://github.com/Cloud-Cuckoo/App/releases/download/#{version}/Cloud-Cuckoo-macOS.zip",
       verified: "github.com/Cloud-Cuckoo/"
