7041193b5a31d8f5866524e55f2f55bde9f4dbd8
appfairbot <appfairbot@appfair.net>
2022-10-14 18:42:14 +0000

    cask update

--- Casks/sun-bow-prerelease.rb
+++ Casks/sun-bow-prerelease.rb
@@ -1,6 +1,6 @@
 cask "sun-bow-prerelease" do
   version "0.0.28"
-  sha256 "5f37c050d1933171466727e6a01f5dc5ff9fd748ce54538076a6d90abad7d973"
+  sha256 "217935a3c15b9b1307a0b46c9d17098d610046118773f9a373533a664e204bbb"
 
   url "https://github.com/Sun-Bow/App/releases/download/#{version}/Sun-Bow-macOS.zip",
       verified: "github.com/Sun-Bow/"
