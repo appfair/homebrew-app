e02e710fc3956b3ca4244c8ed253130fda969f65
appfairbot <appfairbot@appfair.net>
2022-10-18 14:39:38 +0000

    cask update

--- Casks/world-fair-prerelease.rb
+++ Casks/world-fair-prerelease.rb
@@ -1,6 +1,6 @@
 cask "world-fair-prerelease" do
   version "0.0.4"
-  sha256 "81503c980009f95d2ab75f300d2e58f4c5051c1299079cba30157d60e6932f51"
+  sha256 "5ef97eb180e9eff0f27d91d1fea9e202e2e54f3ae8184539064b6dff3d81bcc2"
 
   url "https://github.com/World-Fair/App/releases/download/#{version}/World-Fair-macOS.zip",
       verified: "github.com/World-Fair/"
